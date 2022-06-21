dbf894fcd4404ad0400df5c0be71084832acb694
Gabe Bautista <gabe.bautista1115@gmail.com>
2022-06-21 09:34:08 -0600

    finished madlib

--- Tuesday/MadLib/madlibscript.js
+++ Tuesday/MadLib/madlibscript.js
@@ -2,18 +2,19 @@ let submitButton = document.querySelector(".submit-button");
 submitButton.addEventListener("click", submitClicked);
 
 let word1Area = document.querySelector(".word1Area");
-let word2Area = document.querySelector(".word1Area");
-let word3Area = document.querySelector(".word2Area");
-let word4Area = document.querySelector(".word3Area");
-let word5Area = document.querySelector(".word4Area");
-let word6Area = document.querySelector(".word5Area");
-let word7Area = document.querySelector(".word6Area");
-let word8Area = document.querySelector(".word7Area");
-let word9Area = document.querySelector(".word8Area");
-let word10Area = document.querySelector(".word9Area");
-let word11Area = document.querySelector(".word10Area");
-let word12Area = document.querySelector(".word11Area");
-let word13Area = document.querySelector(".word12Area");
+let word2Area = document.querySelector(".word2Area");
+let word3Area = document.querySelector(".word3Area");
+let word4Area = document.querySelector(".word4Area");
+let word5Area = document.querySelector(".word5Area");
+let word6Area = document.querySelector(".word6Area");
+let word7Area = document.querySelector(".word7Area");
+let word8Area = document.querySelector(".word8Area");
+let word9Area = document.querySelector(".word9Area");
+let word10Area = document.querySelector(".word10Area");
+let word11Area = document.querySelector(".word11Area");
+let word12Area = document.querySelector(".word12Area");
+let word13Area = document.querySelector(".word13Area");
+let madlib = document.querySelector(".madlib");
 
 let word1 = "";
 let word2 = "";
@@ -32,8 +33,8 @@ let word13 = "";
 word1Area.innerHTML = word1;
 console.log(word1);
 
-
 function submitClicked() {
+  madlib.style.display = "block";
   word1 = document.querySelector(".word1").value;
   word2 = document.querySelector(".word2").value;
   word3 = document.querySelector(".word3").value;
@@ -47,5 +48,20 @@ function submitClicked() {
   word11 = document.querySelector(".word11").value;
   word12 = document.querySelector(".word12").value;
   word13 = document.querySelector(".word13").value;
-  window.location.href = "completeLib.html";
+
+  console.log(word1);
+
+  word1Area.textContent = word1;
+  word2Area.textContent = word2;
+  word3Area.textContent = word3;
+  word4Area.textContent = word4;
+  word5Area.textContent = word5;
+  word6Area.textContent = word6;
+  word7Area.textContent = word7;
+  word8Area.textContent = word8;
+  word9Area.textContent = word9;
+  word10Area.textContent = word10;
+  word11Area.textContent = word11;
+  word12Area.textContent = word12;
+  word13Area.textContent = word13;
 }
