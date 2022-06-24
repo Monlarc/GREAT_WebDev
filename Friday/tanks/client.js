c61446ca1b6e9ae48d8b1481466f8013284ad42d
Gabe Bautista <gabe.bautista1115@gmail.com>
2022-06-24 10:49:40 -0600

    added server code

--- Friday/tanks/client.js
+++ Friday/tanks/client.js
@@ -2,76 +2,74 @@
 let socket;
 
 const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext('2d');
+const ctx = canvas.getContext("2d");
 
 let direction = "up";
 let moving = "false";
 
-function move(event){
-    switch(event.key){
-        case 'w':
-            // socket.send("{\"moving\": \"true\", \"dir\":\"up\"}")
-            moving = "true";
-            direction = "up";
-            break;
-        case 'a':
-            // socket.send("{\"moving\": \"true\", \"dir\":\"left\"}")
-            moving = "true";
-            direction = "left";
-            break;
-        case 's':
-            // socket.send("{\"moving\": \"true\", \"dir\":\"down\"}")
-            moving = "true";
-            direction = "down";
-            break;
-        case 'd':
-            // socket.send("{\"moving\": \"true\", \"dir\":\"right\"}")
-            moving = "true";
-            direction = "right";
-            break;
-    }
+function move(event) {
+  switch (event.key) {
+    case "w":
+      // socket.send("{\"moving\": \"true\", \"dir\":\"up\"}")
+      moving = "true";
+      direction = "up";
+      break;
+    case "a":
+      // socket.send("{\"moving\": \"true\", \"dir\":\"left\"}")
+      moving = "true";
+      direction = "left";
+      break;
+    case "s":
+      // socket.send("{\"moving\": \"true\", \"dir\":\"down\"}")
+      moving = "true";
+      direction = "down";
+      break;
+    case "d":
+      // socket.send("{\"moving\": \"true\", \"dir\":\"right\"}")
+      moving = "true";
+      direction = "right";
+      break;
+  }
 }
 
-document.addEventListener('keydown', move);
+document.addEventListener("keydown", move);
 
-
-
-function drawMap(event){
-    // console.log(event.data)
-    const map = JSON.parse(event.data);
-    ctx.fillStyle = "black";
-    ctx.fillRect(0,0,canvas.width, canvas.height);
-    ctx.fillStyle = "white";
-    for(const element of map){
-        ctx.fillRect(element[0], element[1], 10, 10);
-    }
-    // console.log(map);
-
-
+function drawMap(event) {
+  // console.log(event.data)
+  const map = JSON.parse(event.data);
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = "white";
+  for (const element of map) {
+    ctx.fillRect(element[0], element[1], 10, 10);
+  }
+  // console.log(map);
 }
 
-function closeConnection(event){
-    document.getElementById("connectButton").style.visibility = "visible";
+function closeConnection(event) {
+  document.getElementById("connectButton").style.visibility = "visible";
 }
 
-function openConnection() {
-    socket = new WebSocket("ws://10.17.250.208:8080");
-    // Connection opened
-    socket.addEventListener("open", function (event) {
-        console.log("connected to server");
-        document.getElementById("connectButton").style.visibility = "hidden";
-    });
+socket = new WebSocket("wss://127.0.0.1:8080/");
 
-    // Listen for messages
-    socket.addEventListener("message", drawMap);
-    socket.addEventListener("close", closeConnection);
-}
+// Connection opened
+//   socket.addEventListener("open", function (event) {
+//     console.log("connected to server");
+//     document.getElementById("connectButton").style.visibility = "hidden";
+//   });
 
-function pingServer(){
-    moving = "true";
-    socket.send("{\"moving\": \"" + moving + "\", \"dir\": \"" + direction + "\"}");
-    moving = "false";
-}
+// Listen for messages
+//   socket.addEventListener("message", drawMap);
+//   socket.addEventListener("close", closeConnection);
 
-// openConnection()
-setInterval(pingServer, 33);
+socket.onopen = (e) => {
+  socket.send("send to server");
+};
+
+// function pingServer() {
+//   moving = "true";
+//   socket.send('{"moving": "' + moving + '", "dir": "' + direction + '"}');
+//   moving = "false";
+// }
+
+// setInterval(pingServer, 33);
--- Wednesday/RaceGame/racegame.js
+++ Wednesday/RaceGame/racegame.js
@@ -7,8 +7,23 @@ let blueKey = true;
 
 let racing = false;
 
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
+//SERVER CONNECTION CODE
+
+const ws = new WebSocket("ws://localhost:9898/");
+
+ws.onopen = function () {
+  console.log("WebSocket Client Connected");
+  ws.send("Hi this is web client.");
+};
+
+ws.onmessage = function (e) {
+  console.log("Received: '" + e.data + "'");
+};
+
+//END SERVER CONNECTION CODE
+
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
 const resultsDisplay = document.getElementById("results");
 document.addEventListener("keydown", keyboardHandler);
 document.body.style.backgroundColor = "#b3ccf5";
@@ -21,87 +36,88 @@ const finishLine = canvas.width - 15;
 
 let drawID = setInterval(draw, 33);
 
-function startGame(){
-    btn.style.visibility = "hidden";
-    redPos = 10;
-    bluePos = 10;
-    redKey = true;
-    blueKey = true;
+function startGame() {
+  btn.style.visibility = "hidden";
+  redPos = 10;
+  bluePos = 10;
+  redKey = true;
+  blueKey = true;
+  racing = false;
+  countDown(3);
+}
+
+function countDown(time) {
+  if (time == 0) {
+    resultsDisplay.innerHTML = "GO!";
+    racing = true;
+    return;
+  }
+  resultsDisplay.innerHTML = time;
+  setTimeout(countDown, 1000, time - 1);
+}
+
+function drawTrack() {
+  ctx.fillStyle = "gray";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = "#ccac39"; // yellow
+  for (let i = 0; i < canvas.width; i += 14) {
+    ctx.fillRect(i, canvas.height / 2, 7, 2);
+  }
+  ctx.fillStyle = "black";
+  ctx.fillRect(finishLine, 0, 7, canvas.height);
+}
+
+function draw() {
+  drawTrack();
+  ctx.drawImage(redCar, redPos, 7);
+  ctx.drawImage(blueCar, bluePos, 46);
+}
+
+function detectWin() {
+  if (bluePos + 32 > finishLine) {
     racing = false;
-    countDown(3);
+    resultsDisplay.innerHTML = "Blue wins!";
+    btn.innerHTML = "Play again";
+    btn.style.visibility = "visible";
+  } else if (redPos + 32 > finishLine) {
+    racing = false;
+    resultsDisplay.innerHTML = "Red wins!";
+    btn.innerHTML = "Play again";
+    btn.style.visibility = "visible";
+  }
 }
 
-function countDown(time){
-    if(time == 0){
-        resultsDisplay.innerHTML = "GO!";
-        racing = true;
-        return;
-    }
-    resultsDisplay.innerHTML = time;
-    setTimeout(countDown, 1000, time - 1);
-}
+function keyboardHandler(event) {
+  if (!racing) {
+    return;
+  }
 
-function drawTrack(){
-    ctx.fillStyle = "gray";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = "#ccac39"; // yellow
-    for(let i = 0; i < canvas.width; i += 14){
-        ctx.fillRect(i, canvas.height/2, 7, 2);
+  if (event.keyCode == 65) {
+    // a
+    if (redKey) {
+      ws.send("red car moves");
+      redPos += speed;
+      redKey = false;
     }
-    ctx.fillStyle = "black";
-    ctx.fillRect(finishLine, 0, 7, canvas.height);
-}
+  } else if (event.keyCode == 83) {
+    // s
+    if (!redKey) {
+      redPos += speed;
+      redKey = true;
+    }
+  } else if (event.keyCode == 74) {
+    // j
+    if (blueKey) {
+      bluePos += speed;
+      blueKey = false;
+    }
+  } else if (event.keyCode == 75) {
+    // k
+    if (!blueKey) {
+      bluePos += speed;
+      blueKey = true;
+    }
+  }
 
-function draw(){
-    drawTrack();
-    ctx.drawImage(redCar, redPos, 7);
-    ctx.drawImage(blueCar, bluePos, 46);
-}
-
-function detectWin(){
-    if(bluePos + 32 > finishLine){
-        racing = false;
-        resultsDisplay.innerHTML = "Blue wins!";
-        btn.innerHTML = "Play again";
-        btn.style.visibility = "visible";
-    }
-    else if(redPos + 32 > finishLine){
-        racing = false;
-        resultsDisplay.innerHTML = "Red wins!";
-        btn.innerHTML = "Play again";
-        btn.style.visibility = "visible";
-    }
-}
-
-function keyboardHandler(event){
-    if(!racing){
-        return;
-    }
-    
-    if(event.keyCode == 65){ // a
-        if(redKey){
-            redPos += speed;
-            redKey = false;
-        }
-    }
-    else if(event.keyCode == 83){ // s
-        if(!redKey){
-            redPos += speed;
-            redKey = true;
-        }
-    }
-    else if(event.keyCode == 74){ // j
-        if(blueKey){
-            bluePos += speed;
-            blueKey = false;
-        }
-    }
-    else if(event.keyCode == 75){ // k
-        if(!blueKey){
-            bluePos += speed;
-            blueKey = true;
-        }
-    }
-
-    detectWin();
+  detectWin();
 }
